5f397a4b407a66d8717a57c3cf05dd619ed19e11
Chloe Dawn <chloe@sapphic.dev>
2020-06-25 18:50:40 +0100

    Introduce support for pre-GLFW game versions  - Group behaviour is a bit strange, needs review

---
+++ src/main/java/dev/sapphic/controlshift/JInput.java
@@ -0,0 +1,16 @@
+package dev.sapphic.controlshift;
+
+import org.jetbrains.annotations.ApiStatus;
+
+@ApiStatus.Internal
+interface JInput {
+  /**
+   * The JInput left control key index
+   */
+  int KEY_LEFTCTRL = 29;
+
+  /**
+   * The JInput left shift key index
+   */
+  int KEY_LEFTSHIFT = 42;
+}
